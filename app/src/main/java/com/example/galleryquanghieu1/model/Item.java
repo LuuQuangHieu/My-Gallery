ce883c8455d57750181ee290e77539bc21902900
LuuQuangHieu <hieuluu.work@gmail.com>
2022-09-04 17:52:17 +0700

    first commit

---
+++ app/src/main/java/com/example/galleryquanghieu1/model/Item.java
@@ -0,0 +1,11 @@
+package com.example.galleryquanghieu1.model;
+
+public class Item {
+    String itemPath;
+    String itemSize;
+    String itemName;
+    String itemType;
+    String itemTime;
+    boolean itemFavorite;
+    boolean itemBin;
+}
